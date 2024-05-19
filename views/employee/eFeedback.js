2c8ad43edb829b22f24670e4984516c830850d78
dumaMandimo <2548972@students.wits.ac.za>
2024-05-19 14:47:38 +0200

    Sending feedback and receiving an alert

--- views/employee/eFeedback.js
+++ views/employee/eFeedback.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCvnZkbqON0vsIackr90txDbg-oYj_ikJ0",
@@ -29,40 +29,43 @@ function showAlert(message, className) {
 
 
 // Function to add employee feedback to Firebase database
+//Function to add employee feedback to Firebase database
 function addFeedbackToDatabase() {
     const yourName = document.querySelector("#yourName").value;
     const employeeName = document.querySelector("#employeeName").value;
     const employeeStars = document.querySelector('input[name="rate"]:checked');
 
-    //Exit the function if star rating is not provided
+    // Exit the function if star rating is not provided
     if (!employeeStars) {
         showAlert("Please select a star rating", "danger");
         return; 
     }
-
-   
 
     // Push feedback object to Firebase database
     push(feedbackRef, {
         yourName: yourName,
         employeeName: employeeName,
         employeeStars: employeeStars.value
+    }).then(() => {
+        // Show success alert
+        showAlert("Feedback Submitted!", "success");
 
+        // Clear input fields
+        document.querySelector("#yourName").value = "";
+        document.querySelector("#employeeName").value = "";
+        const starInputs = document.querySelectorAll('input[name="rate"]');
+        starInputs.forEach(input => {
+            input.checked = false;
         });
-    // Show success alert
-    showAlert("Feedback Added!", "success");
-
-    // Clear input fields
-    document.querySelector("#yourName").value = "";
-    document.querySelector("#employeeName").value = "";
-    const starInputs = document.querySelectorAll('input[name="rate"]');
-    starInputs.forEach(input => {
-        input.checked = false;
+    }).catch((error) => {
+        // Handle errors
+        console.error('Error adding feedback:', error);
+        showAlert('Error adding feedback. Please try again later.', 'danger');
     });
 }
 
-
 // Event listener for form submission
-document.querySelector("#feedbackForm").addEventListener("submit", () => {
+document.querySelector("#feedbackForm").addEventListener("submit", (e) => {
+    e.preventDefault();
     addFeedbackToDatabase();
 });
